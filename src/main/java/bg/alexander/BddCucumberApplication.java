a947aa2ee6008b0a5b7c0f1e4f3bf7407d1e47fd
KIRILOV Alexander <alexander.kirilov.devoteam@axa.fr>
2017-03-23 15:53:46 +0100

    Spring Boot Init

---
+++ src/main/java/bg/alexander/BddCucumberApplication.java
@@ -0,0 +1,12 @@
+package bg.alexander;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BddCucumberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BddCucumberApplication.class, args);
+	}
+}
